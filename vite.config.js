b52376a904f8c55f634b41a44cc0d09283d21266
yahya <307878586+yahya2472008-maker@users.noreply.github.com>
2026-09-24 08:28:51 +0700

    Configure Vite for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-﻿import { defineConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/noteflow/",
-  css: {
-    postcss: false
-  }
+  base: "/noteflow/"
 });
